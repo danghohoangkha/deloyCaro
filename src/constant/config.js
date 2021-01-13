157544177daa6e4635cb0e000aa256edecfac210
danghohoangkha <danghohoangkha2017@gmail.com>
2021-01-13 23:21:00 +0700

    deloy3

--- src/constant/config.js
+++ src/constant/config.js
@@ -1,2 +1,3 @@
 export const API_TEMP = 'https://5fc48bd836bc7900163439dc.mockapi.io/api'
 export const Backend_API = 'https://caro-admin-api-482-509-528.herokuapp.com'
+export const URL_Website = 'http://localhost:3000'
